1f0de5f0dc921728d085f49c20fed3350dd32d09
vladveterok <vl.berezhnoi@gmail.com>
2020-06-30 18:20:57 +0300

    Require user.rb and file_loader.rb from codebreaker.rb, wrap user.rb and file_loader.rb into Codebreaker module

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require 'codebreaker/version'
+require 'codebreaker/modules/file_loader'
 require 'codebreaker/game'
+require 'codebreaker/user'
+
 
 module Codebreaker
   class Error < StandardError; end
--- lib/codebreaker/user.rb
+++ lib/codebreaker/user.rb
@@ -1,49 +1,51 @@
 # frozen_string_literal: true
 
 # Needs a class documentation
-class User
-  attr_reader :name
-  attr_reader :difficulty
-  attr_reader :attempts
-  attr_reader :attempts_used
-  attr_reader :number_of_hints
-  attr_reader :hints_used
+module Codebreaker
+  class User
+    attr_reader :name
+    attr_reader :difficulty
+    attr_reader :attempts
+    attr_reader :attempts_used
+    attr_reader :number_of_hints
+    attr_reader :hints_used
 
-  def initialize(name:, game:)
-    validate(name: name, game: game) # add all attributes
-    @name = name
-    @game = game
+    def initialize(name:, game:)
+      validate(name: name, game: game) # add all attributes
+      @name = name
+      @game = game
 
-    @difficulty = @game.difficulty
-    @attempts = @game.attempts
-    @number_of_hints = @game.number_of_hints
+      @difficulty = @game.difficulty
+      @attempts = @game.attempts
+      @number_of_hints = @game.number_of_hints
 
-    @attempts_used = 0
-    @hints_used = 0
-  end
+      @attempts_used = 0
+      @hints_used = 0
+    end
 
-  def save_results
-    @attempts_used = @attempts - @game.attempts
-    @hints_used = @number_of_hints - @game.number_of_hints
-    # save results from game to here
-  end
+    def save_results
+      @attempts_used = @attempts - @game.attempts
+      @hints_used = @number_of_hints - @game.number_of_hints
+      # save results from game to here
+    end
 
-  private
+    private
 
-  def validate(arguments)
-    validate_name(arguments[:name])
-  end
+    def validate(arguments)
+      validate_name(arguments[:name])
+    end
 
-  def validate_name(name)
-    raise ArgumentError, 'Name should be of 3 to 20 characters' if name.length < 3 || name.length > 20
-  end
+    def validate_name(name)
+      raise ArgumentError, 'Name should be of 3 to 20 characters' if name.length < 3 || name.length > 20
+    end
 
-  def to_s
-    "name: #{@name} " \
-    "difficulty: #{@difficulty} " \
-    "attempts total #{@attempts} " \
-    "attempts used: #{@attempts_used} " \
-    "hints total: #{@number_of_hints} " \
-    "hints used: #{@hints_used}"
+    def to_s
+      "name: #{@name} " \
+      "difficulty: #{@difficulty} " \
+      "attempts total #{@attempts} " \
+      "attempts used: #{@attempts_used} " \
+      "hints total: #{@number_of_hints} " \
+      "hints used: #{@hints_used}"
+    end
   end
 end
