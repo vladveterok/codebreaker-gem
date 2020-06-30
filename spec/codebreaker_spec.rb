0136969485bdc2b96acc5723f9cff3037bd6330a
vladveterok <vl.berezhnoi@gmail.com>
2020-06-30 18:55:52 +0300

    Add some minor changes to codebreaker_spec.rb

--- spec/codebreaker_spec.rb
+++ spec/codebreaker_spec.rb
@@ -11,16 +11,16 @@ RSpec.describe Codebreaker do
     it 'creates a new game' do
       expect(game).to be_an_instance_of(Codebreaker::Game)
     end
-  end
-
-  context 'when new game is created' do
-    it 'creates a game of particular difficulty' do
-      expect(game.difficulty).to be('easy')
-    end
 
     it 'creates a new user' do
       expect(game.user).to be_an_instance_of(Codebreaker::User)
       expect(game.user.name).to be('Foobar')
+    end
+  end
+
+  context 'when new game is created' do
+    it 'is a game of particular difficulty' do
+      expect(game.difficulty).to be('easy')
     end
   end
 end
