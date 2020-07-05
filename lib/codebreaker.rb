6d689ce9910209162c0354d2594d119894ff33ab
vladveterok <vl.berezhnoi@gmail.com>
2020-07-05 23:57:15 +0300

    Move requerements into codebreaker.rb

--- lib/codebreaker.rb
+++ lib/codebreaker.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
-require './bootstrap'
+require_relative 'codebreaker/version'
+require 'yaml/store'
+require 'pathname'
+
+require_relative 'codebreaker/modules/validation'
+require_relative 'codebreaker/modules/file_loader'
+require_relative 'codebreaker/game'
+require_relative 'codebreaker/matchmaker'
+require_relative 'codebreaker/user'
 
 module Codebreaker
   class Error < StandardError; end
