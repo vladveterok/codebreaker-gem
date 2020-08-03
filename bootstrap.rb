b35f25089e9f8bdb57f3ed0833e2ad8e5802d46b
vladveterok <vl.berezhnoi@gmail.com>
2020-08-03 23:45:10 +0300

    Remix requirements in bootstrap

--- bootstrap.rb
+++ bootstrap.rb
@@ -5,9 +5,9 @@ require 'pathname'
 
 require_relative 'lib/codebreaker/version'
 
-require_relative 'lib/codebreaker'
 require_relative 'lib/codebreaker/modules/validation'
 require_relative 'lib/codebreaker/modules/file_loader'
-require_relative 'lib/codebreaker/game'
 require_relative 'lib/codebreaker/matchmaker'
 require_relative 'lib/codebreaker/user'
+require_relative 'lib/codebreaker/game'
+require_relative 'lib/codebreaker'
