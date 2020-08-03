60b5aeec9fb88762ca8403a7c3c1f3f158b1c874
vladveterok <vl.berezhnoi@gmail.com>
2020-08-03 23:42:53 +0300

    Add bootstrap.rb, move requirements from lib/codebreaker.rb to bootstrap.rb

---
+++ bootstrap.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'yaml/store'
+require 'pathname'
+
+require_relative 'lib/codebreaker/version'
+
+require_relative 'lib/codebreaker'
+require_relative 'lib/codebreaker/modules/validation'
+require_relative 'lib/codebreaker/modules/file_loader'
+require_relative 'lib/codebreaker/game'
+require_relative 'lib/codebreaker/matchmaker'
+require_relative 'lib/codebreaker/user'
