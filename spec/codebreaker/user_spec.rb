4a046ec32e406ac85c8f58cfd6169d7ac289b805
vladveterok <vl.berezhnoi@gmail.com>
2020-08-16 18:07:27 +0300

    Refactor user_spec.rb, split logic into two contexts

--- spec/codebreaker/user_spec.rb
+++ spec/codebreaker/user_spec.rb
@@ -1,17 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe Codebreaker::User do
+  subject(:call) { described_class.new(name: name) }
+
   describe '#initialize' do
-    context 'when name is invalid' do
-      let(:name_too_short) { 'a' * (Codebreaker::User::NAME_LENGTH.min - 1) }
-      let(:name_too_long) { 'a' * (Codebreaker::User::NAME_LENGTH.max + 1) }
+    context 'when name is too short' do
+      let(:name) { 'a' * (Codebreaker::User::NAME_LENGTH.min - 1) }
 
       it 'raises ArgumentError when less than 3 characters' do
-        expect { described_class.new(name: name_too_short) }.to raise_error(described_class::InvalidName)
+        expect { call }.to raise_error(described_class::InvalidName)
       end
+    end
+
+    context 'when name is too long' do
+      let(:name) { 'a' * (Codebreaker::User::NAME_LENGTH.max + 1) }
 
       it 'raises ArgumentError when more than 20 characters' do
-        expect { described_class.new(name: name_too_long) }.to raise_error(described_class::InvalidName)
+        expect { call }.to raise_error(described_class::InvalidName)
       end
     end
   end
