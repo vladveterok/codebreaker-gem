40744c37b4691f0ca532e9e5b673ef6c6a70fa92
vladveterok <vl.berezhnoi@gmail.com>
2020-08-19 12:54:17 +0300

    Cleanup project

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@
 require 'simplecov'
 SimpleCov.start
 
-# Substitutes name for a db file 'results.yml' while testing
 ENV['DB_PATH'] = "#{Pathname(__FILE__).parent.dirname.realpath}/db/"
 ENV['DB_FILE'] = 'results_test.yml'
 
@@ -11,10 +10,8 @@ require 'bundler/setup'
 require 'codebreaker'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
